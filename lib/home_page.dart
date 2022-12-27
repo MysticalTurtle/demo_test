f69d7eac1464d81b2432a199414087bc11327a81
Bitel Mac <mibitelios2022@gmail.com>
2022-12-27 15:26:33 -0500

    feat: add upload measurement

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
@@ -14,8 +15,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double _value = 0;
-  double velocity1 = 0;
-  double velocity2 = 0;
+  double _valueUpload = 0;
+  double velocityDL1 = 0;
+  double velocityDL2 = 0;
+
+  double velocityUL = 0;
   double lostTime = 0;
   Dio dio = Dio();
 
@@ -24,10 +28,63 @@ class _HomePageState extends State<HomePage> {
   void _restartValues() {
     setState(() {
       _value = 0;
-      velocity1 = 0;
-      velocity2 = 0;
+      _valueUpload = 0;
+      velocityDL1 = 0;
+      velocityDL2 = 0;
+
+      velocityUL = 0;
+
       lostTime = 0;
     });
+  }
+
+  void _startUpload() async {
+    tempDir = "${(await getTemporaryDirectory()).path}test.bin";
+
+    int currentTime = 0;
+    double totalTime = 0;
+
+    int packageSize = 5 * 1024 * 1024;
+
+    Uint8List bytesToUpload = Uint8List(packageSize);
+    File fileToUpload = File(tempDir)..writeAsBytesSync(bytesToUpload);
+
+    FormData dataToSend = FormData.fromMap({
+      "file":
+          await MultipartFile.fromFile(fileToUpload.path, filename: "test.bin")
+    });
+
+    print("Starting uploading");
+
+    int startDownloadTime = DateTime.now().millisecondsSinceEpoch;
+    await dio.post(
+      "http://181.176.254.106:8165/qos-service/api/file/upload",
+      data: dataToSend,
+      options: Options(headers: {HttpHeaders.acceptEncodingHeader: "*"}),
+      onSendProgress: (count, total) {
+        currentTime = DateTime.now().millisecondsSinceEpoch;
+
+        // if (!firstPackedReceived) {
+        //   firstPackedReceived = true;
+        //   startDownloadTime2 = currentTime;
+        //   firstPackedSize = count;
+        //   lostTime = (currentTime - startDownloadTime1) / 1000;
+        //   setState(() {});
+        // }
+
+        totalTime = (currentTime - startDownloadTime).toDouble() / 1000;
+
+        debugPrint("UL>>> Count and total: $count / $total");
+        debugPrint("UL>>> Ratio: ${count / total}");
+        debugPrint("UL>>> Total Time2: $totalTime");
+        setState(() {
+          velocityUL = (count / totalTime / 1024 / 1024);
+          _valueUpload = count / total;
+        });
+      },
+    )
+        // .timeout(const Duration(seconds: 21))
+        .whenComplete(() {});
   }
 
   void _startDownload() async {
@@ -68,8 +125,8 @@ class _HomePageState extends State<HomePage> {
         debugPrint("DL>>> Total Time1: $totalTime1");
         debugPrint("DL>>> Total Time2: $totalTime2");
         setState(() {
-          velocity1 = (count / totalTime1 / 1024 / 1024);
-          velocity2 = ((count - firstPackedSize) / totalTime2 / 1024 / 1024);
+          velocityDL1 = (count / totalTime1 / 1024 / 1024);
+          velocityDL2 = ((count - firstPackedSize) / totalTime2 / 1024 / 1024);
           _value = count / total;
         });
       },
@@ -92,6 +149,10 @@ class _HomePageState extends State<HomePage> {
             child: const Text("Download Test"),
           ),
           ElevatedButton(
+            onPressed: _startUpload,
+            child: const Text("Upload Test"),
+          ),
+          ElevatedButton(
             onPressed: _restartValues,
             child: const Text("Restart Test"),
           ),
@@ -110,13 +171,31 @@ class _HomePageState extends State<HomePage> {
             children: [
               const Text("Download rate (MB/s)"),
               Text(
-                  "Velocidad fórmula 1: ${(velocity1 * 8).toStringAsFixed(2)}"),
+                  "Velocidad fórmula 1: ${(velocityDL1 * 8).toStringAsFixed(2)}"),
               Text(
-                  "Velocidad fórmula 2: ${(velocity2 * 8).toStringAsFixed(2)}"),
+                  "Velocidad fórmula 2: ${(velocityDL2 * 8).toStringAsFixed(2)}"),
               const SizedBox(height: 20),
               lostTime != 0 ? Text("Lost time: $lostTime") : Container(),
             ],
-          )
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: LinearPercentIndicator(
+                  lineHeight: 20.0,
+                  percent: _valueUpload,
+                  center: Text("${(_valueUpload * 100).toStringAsFixed(1)}%"),
+                  // linearStrokeCap: LinearStrokeCap.roundAll,
+                  progressColor: Colors.purple,
+                ),
+              ),
+              const Text("Upload rate (MB/s)"),
+              Text(
+                  "Velocidad fórmula 1: ${(velocityUL * 8).toStringAsFixed(2)}"),
+              const SizedBox(height: 20),
+            ],
+          ),
         ],
       ),
     );
